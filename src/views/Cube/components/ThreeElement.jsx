659e21d901f6a7ba7e365cbefacf8d739a9e9b2f
Moazam05 <salmanmoazam08@gmail.com>
2024-06-29 13:21:19 +0500

    mesh light

--- src/views/Cube/components/ThreeElement.jsx
+++
@@ -1,5 +0,0 @@
-const ThreeElement = () => {
-  return <div>ThreeElement</div>;
-};
-
-export default ThreeElement;
--- src/views/Cube/index.jsx
+++ src/views/Cube/index.jsx
@@ -1,13 +1,15 @@
 import { Canvas } from "@react-three/fiber";
-import ThreeElement from "./components/ThreeElement";
+import { MathUtils } from "three";
 
 const Cube = () => {
   return (
-    <>
-      <Canvas>
-        <ThreeElement />
-      </Canvas>
-    </>
+    <Canvas>
+      <directionalLight position={[5, 5, 5]} />
+      <mesh rotation={[MathUtils.degToRad(45), MathUtils.degToRad(45), 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="red" />
+      </mesh>
+    </Canvas>
   );
 };
 
